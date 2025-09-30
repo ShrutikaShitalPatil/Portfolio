6ab495cdd1dc2954af3ace61356dd2429af1157d
Shrutika Patil <146436779+ShrutikaShitalPatil@users.noreply.github.com>
2025-09-30 21:57:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
+
+// let nav=document.querySelector('nav');
+
+// nav.classList.toggle('sticky',window.scrollY >100);
+
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
